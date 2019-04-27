885de6d3220b18eae00173a2cc8aa93a8ee7776b
Zarosch <kevin.velz@gmail.com>
2019-04-27 18:10:47 +0200

    :star: Improve MessageUtil

--- plugin/src/main/java/me/velz/shops/utils/MessageUtil.java
+++ plugin/src/main/java/me/velz/shops/utils/MessageUtil.java
@@ -1,49 +1,65 @@
 package me.velz.shops.utils;
 
 import lombok.Getter;
+import me.velz.shops.Shops;
 
 public enum MessageUtil {
 
     // Prefix Messages
-    PREFIX("§f[§eShops§f] "),
+    PREFIX("", "§f[§eShops§f] ", "§f[§eShops§f] "),
     // Error Messages
-    ERROR_NOPERMISSIONS("§cDazu hast du keine Berechtigung."),
-    ERROR_PLAYERONLY("§cDieser Befehl kann nur von einem Spieler genutzt werden."),
-    ERROR_PLAYERNOTFOUND("§cDieser Spieler konnte nicht gefunden werden."),
-    ERROR_NONUMBER("§cDu musst eine gültige Zahl angeben."),
-    ERROR_NOITEMINHAND("§cDu hast kein Item in der Hand."),
-    ERROR_SYNTAX("§cSyntax Fehler, bitte nutze: %command"),
-    
-    SHOP_NOTENOUGH("§cDu hast nicht genügend Geld."),
-    SHOP_DISABLE("§6Alle Shops wurden §cdisable§6."),
-    SHOP_ACTIVATE("§6Alle Shops wurden §aaktiviert§6."),
-    SHOP_RELOAD("§aDie Konfiguration wurde neugeladen."),
-    SHOP_NOTENOUGHITEM("§cDu hast nicht genügend Items."),
-    
-    COMMAND_HELP_HEAD("§f§lShops Befehle"),
-    COMMAND_HELP_DISABLE("§6/shops disable &fShops deaktivieren"),
-    COMMAND_HELP_RELOAD("§6/shops reload &fKonfiguration neuladen");
+    ERROR_NOPERMISSIONS("", "§cDazu hast du keine Berechtigung.", "§cYou dont have permissions."),
+    ERROR_PLAYERONLY("", "§cDieser Befehl kann nur von einem Spieler genutzt werden.", "§cThis command can only used by players."),
+    ERROR_PLAYERNOTFOUND("", "§cDieser Spieler konnte nicht gefunden werden.", "§cThis player can not be found."),
+    ERROR_NONUMBER("", "§cDu musst eine gültige Zahl angeben.", "§cYou have to enter a valid number."),
+    ERROR_NOITEMINHAND("", "§cDu hast kein Item in der Hand.", "§cYou don't have a Item in your hand."),
+    ERROR_SYNTAX("", "§cSyntax Fehler, bitte nutze: %command", "§cSyntax Error, please use: %command"),
+    SHOP_NOTENOUGH("", "§cDu hast nicht genügend Geld.", "§cYou dont have enought money."),
+    SHOP_DISABLE("", "§6Alle Shops wurden §cdeaktiviert§6.", "§6All Shops are now §cdisabled§6."),
+    SHOP_ACTIVATE("", "§6Alle Shops wurden §aaktiviert§6.", "§6All Shops are now §aenabled§6."),
+    SHOP_RELOAD("", "§aDie Konfiguration wurde neugeladen.", "§aThe configuration was reloaded."),
+    SHOP_NOTENOUGHITEM("", "§cDu hast nicht genügend Items.", "§cYou dont have enough items."),
+    COMMAND_HELP_HEAD("", "§f§lShops Befehle", "§f§lShops Commands"),
+    COMMAND_HELP_DISABLE("", "§6/shops disable §fShops deaktivieren", "§6/shops disable §fDisable shops"),
+    COMMAND_HELP_RELOAD("", "§6/shops reload §fKonfiguration neuladen", "§6/shops reload §fReload configurations");
 
     @Getter
     private String local;
+    
+    @Getter
+    private final String german, english;
 
-    private MessageUtil(String local) {
+    private MessageUtil(String local, String german, String english) {
         this.local = local;
+        this.german = german;
+        this.english = english;
     }
 
     public static void load() {
-        FileBuilder message = new FileBuilder("plugins/Quests", "messages.yml");
+        FileBuilder message_en = new FileBuilder("plugins/Crates", "messages_en.yml");
         for (MessageUtil m : MessageUtil.values()) {
-            message.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.local.replaceAll("§", "&"));
+            message_en.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.english.replaceAll("§", "&"));
         }
-        message.save();
+        message_en.save();
+
+        FileBuilder message_de = new FileBuilder("plugins/Crates", "messages_de.yml");
+        for (MessageUtil m : MessageUtil.values()) {
+            message_de.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.german.replaceAll("§", "&"));
+        }
+        message_de.save();
+
+
+        FileBuilder message = new FileBuilder("plugins/Crates", "messages_" + Shops.getPlugin().getFileManager().getConfig().getString("language") + ".yml");
+        if (!message.getFile().exists()) {
+            message = new FileBuilder("plugins/Crates", "messages_" + Shops.getPlugin().getFileManager().getConfig().getString("language") + ".yml");
+        }
+        for (MessageUtil m : MessageUtil.values()) {
+            message.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.english.replaceAll("§", "&"));
+        }
         for (MessageUtil m : MessageUtil.values()) {
             m.local = message.getConfiguration().getString("message." + m.toString().replaceAll("_", ".").toLowerCase()).replaceAll("&", "§");
         }
-    }
-
-    public static void save(MessageUtil m) {
-        FileBuilder message = new FileBuilder("plugins/Quests", "messages.yml");
+        message.save();
     }
 
 }
